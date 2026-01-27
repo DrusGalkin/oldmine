7b3c77d7ab1b3a2b3080ae5be6fd42378c1743fb
DrusGalkin <sveta.galkina2021@gmail.com>
2026-01-28 01:28:50 +0300

    первый тест электрона

---
+++ src/renderer/components/background-manager.js
@@ -0,0 +1,7 @@
+const main = document.querySelector('#main');
+
+main.style.backgroundImage = "url('assets/images/background.png')";
+main.style.backgroundSize = "cover";
+main.style.backgroundPosition = "center";
+main.style.backgroundRepeat = "no-repeat";
+main.style.minHeight = "100vh";
